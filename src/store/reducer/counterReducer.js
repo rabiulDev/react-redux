cd7088b7e78963b43d560e9da51f0232d0d07eff
Rabiul Islam <rabiul.dev@gmail.com>
2022-08-16 12:37:39 +0600

    create counter reducer

---
+++ src/store/reducer/counterReducer.js
@@ -0,0 +1,25 @@
+const initialCount = {
+  count: 0,
+};
+
+const counterReducer = (state = initialCount, action) => {
+  switch (action.type) {
+    case INCREAMENT_COUNTER:
+      return { ...state, count: state.count + 1 };
+
+    case DECREAMENT_COUNTER:
+      let decreamentCount;
+      if (state.count > 0) {
+        decreamentCount = state.count - 1;
+      }
+      return { ...state, count: decreamentCount };
+
+    case RESET_COUNTER:
+      return { ...state, count: 0 };
+
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
